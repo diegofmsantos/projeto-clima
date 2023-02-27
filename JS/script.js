00ebbe1ffafd94a3279232e41dd86116511d5a1f
Diego Santos <diegofmsantos0@gmail.com>
2023-02-26 21:38:04 -0300

    projeto finalizado

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,58 @@
-// ff068d1db5c1b8539a19cb69f23f5b2e
+// VARIÁVEIS E SELEÇÃO DE ELEMENTOS
+
+const apiKey = "ff068d1db5c1b8539a19cb69f23f5b2e"
+const apiCountryURL = "https://flagsapi.com/BR/flat/64.png"
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherIconElement = document.querySelector('#weather-icon')
+const humidityElement = document.querySelector('#humidity span')
+const windElement = document.querySelector('#wind span')
+const weatherContainer = document.querySelector('#weather-data')
+
+
+// FUNÇÕES
+
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data
+}
+
+
+const showWeatherData = async   (city) => {
+   const data = await getWeatherData(city)
+
+   cityElement.innerText = data.name
+   tempElement.innerText = parseInt(data.main.temp)
+   descElement.innerText = data.weather[0].description
+   weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+   humidityElement.innerText = `${data.main.humidity}%`
+   windElement.innerText = `${data.wind.speed}km/h`
+
+   weatherContainer.classList.remove("hide")
+
+}
+
+// EVENTOS
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const city = cityInput.value
+
+    showWeatherData(city)
+})
+
+cityInput.addEventListener('keyup', (e) => {
+    if(e.code === "Enter") {
+        const city = e.target.value
+        showWeatherData(city)
+    }
+})
